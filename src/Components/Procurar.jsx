2c796db8307683afd7ac20ebf3215ea80b914fbe
CaioMaile <caiomailesla@gmail.com>
2024-04-11 18:11:37 -0300

    Barra de busca

--- src/Components/Procurar.jsx
+++ src/Components/Procurar.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 //import FilmeModelo from '../Data/FilmeModelo';
-//import Axios from 'axios';
+import axios from 'axios';
 
 export default function Procurar() {
 
+    
+
     return(
         <div style={{ backgroundImage: "url('../../templatemo_556_catalog_z/templatemo_556_catalog_z/img/hero.jpg')" }} className="tm-hero d-flex justify-content-center align-items-center" data-parallax="scroll" >
-            <form className="d-flex tm-search-form" name='q' method='get' action='http://10.0.0.163/api/v1/movies' >
+            <form className="d-flex tm-search-form">
                 <input 
                 className="form-control tm-search-input" 
                 type="search" 
                 placeholder="Search" 
                 aria-label="Search"
                 name='title_cont'
+                // onChange={Entrada}
                 />
                 <button className="btn btn-outline-success tm-search-btn" type="submit">
                     <i className="fas fa-search"></i>
